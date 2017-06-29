0bd3d290158050e70e1e3c26d18975a8dee5d53f
william <william@atac-grazielle.enf.usp>
2017-06-29 07:17:02 -0300

    Inserido Exemplo do segundo tipo, "mudar CSS Class".

--- ApJavaScript/WebContent/teste1.js
+++ ApJavaScript/WebContent/teste1.js
@@ -2,9 +2,16 @@
  * http://usejsdoc.org/
  */
 
+/*Usado no primeiro Exemplo*/
 function mudaHTML() {
 	
 	document.getElementById("parag").innerHTML = "alterado com sucesso!";
 	
+}
+
+/*Usado no segundo Exemplo*/
+function mudaClass() {
+	
+	document.getElementById("mudaClass").className="alterado"; 
 	
 }
